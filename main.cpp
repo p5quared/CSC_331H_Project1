0f31b04ebefdb2b9f07ca7cb942e82b1f6704dec
Peter V <98245483+p5quared@users.noreply.github.com>
2023-02-19 09:44:46 -0500

    bulk prettification.

--- main.cpp
+++ main.cpp
@@ -1,82 +1,90 @@
 #include <iostream>
+#include <iomanip>
 #include "DoublyLinkedList.h"
 
-
-
+#define pause(); std::cin.get();
 
 using std::cout;
 using std::endl;
 
+
 namespace pFuncs
 {
+    // helper functions
     template<typename t>
-    void print(const std::string& msg="", const DoublyLinkedList<t>& l=DoublyLinkedList<t>());
+    void print(const std::string& msg="", DoublyLinkedList<t> l=DoublyLinkedList<t>());
+
+    template<typename t>
+    void getData(const std::string&, t& data);
 
     void displayMenu();
+    void clearDisplay();
+
 }
 
 
-
 int main() {
-    DoublyLinkedList<int> someList = DoublyLinkedList<int>();
+    DoublyLinkedList<int> nummies = DoublyLinkedList<int>();
 
-    cout << "Welcome to the Doubly Linked List Program!" << endl;
-    cout << "This program will allow you to create a Doubly Linked List and manipulate it." << endl;
-    cout << "You can append, prepend, and delete data from the list." << endl;
+    cout << "Welcome to the Doubly Linked List Program!\n";
+    cout << "This program will allow you to create a Doubly Linked List and manipulate it.\n";
+    cout << "You can append, prepend, and delete data from the list.\n";
+    cout << "The first data you enter will set the type of data the list will hold." << endl;
+    cout << "\nPress enter to continue...\n";
+    pause();
 
     // Continuously take user input and execute commands.
-    while (true) {
+    char input;
+    do {
+        pFuncs::clearDisplay();
+        pFuncs::print("List state: ", nummies);
+        cout << "\n";
         pFuncs::displayMenu();
-        char input;
-        std::cin >> input;
+        pFuncs::getData((std::string) "", input);
         switch (input) {
+            int data;
             case 'a':
-                cout << "Enter data to append: ";
-                int data;
-                std::cin >> data;
-                someList.append(data);
+                pFuncs::getData("Enter data to append:", data);
+                nummies.append(data);
                 break;
             case 'p':
-                cout << "Enter data to prepend: ";
-                int data2;
-                std::cin >> data2;
-                someList.prepend(data2);
+                pFuncs::getData("Enter data to prepend:", data);
+                nummies.prepend(data);
                 break;
             case 'd':
-                cout << "Enter data to delete: ";
-                int data3;
-                std::cin >> data3;
-                someList.remove(data3);
+                pFuncs::getData("Enter data to delete:", data);
+                nummies.remove(data);
                 break;
-            case 'q':
-                cout << "Quitting..." << endl;
-                return 0;
             default:
-                cout << "Invalid input. Please try again." << endl;
+                cout << "Command not recognized. Please try again." << endl;
                 break;
         }
         cout << "\n";
-        pFuncs::print("List state: ", someList);
-        cout << "\n";
-    }
-
+    } while (input != 'q');
+    pFuncs::clearDisplay();
+    cout << "Thank you for using the Doubly Linked List Program!\n";
+    pFuncs::print("Final list state: ", nummies);
 
     return 0;
 }
 
 
 template<typename t>
-void pFuncs::print(const std::string& msg, const DoublyLinkedList<t>& l){
-    if(l.isEmpty())
-        cout << "[]" << endl;
+void pFuncs::print(const std::string& msg, DoublyLinkedList<t> l){
+    using std::setw;
+    using std::setfill;
     if(!msg.empty())
         cout << msg << endl;
 
-    cout << "[";
+    std::string output = "[";
     for (auto i : l) {
-        cout << i << ", ";
+        output += std::to_string(i) + ", ";
     }
-    cout << "\b\b]" << endl;
+    output.length() > 1 ? output += "\b\b]"
+            : output += "]";
+    cout << output;
+
+
 }
 
 void pFuncs::displayMenu(){
@@ -84,5 +92,23 @@ void pFuncs::displayMenu(){
     cout << "a. Append data\n";
     cout << "p. Prepend data\n";
     cout << "d. Delete data\n";
-    cout << "q. Quit\n>";
+    cout << "q. Quit" << endl;
+}
+
+
+void pFuncs::clearDisplay(){
+    // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
+    std::cout << "\x1B[2J\x1B[H";
+}
+
+template<typename t>
+void pFuncs::getData(const std::string& msg, t& data) {
+    std::cout << msg << "\n>>";
+    for (;;) {
+        if (std::cin >> data)
+            break;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input. Please try again:\n>>";
+    }
 }
